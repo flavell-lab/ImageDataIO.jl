5c3fd0b5ef1ccdf40fa2ca6481f5e92fbaf57716
goldenratio1618 <adamatanas@gmail.com>
2020-05-20 12:49:27 -0400

    Updated exported functions

--- src/ImageDataIO.jl
+++ src/ImageDataIO.jl
@@ -5,6 +5,15 @@ using HDF5, MHDIO
 include("centroids_io.jl")
 include("worm_features_io.jl")
 
-export load_registration_problems, read_head_pos, write_centroids,
-        read_centroids_transformix, read_centroids_roi, read_mhd
+export
+        load_registration_problems,
+        read_head_pos,
+        write_centroids,
+        read_centroids_transformix,
+        read_centroids_roi,
+        read_mhd,
+        load_training_set,
+        load_predictions,
+        read_activity,
+        write_activity
 end # module
