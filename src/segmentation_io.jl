38e7405cf384193be5e00448128d92554f2f9364
goldenratio1618 <adamatanas@gmail.com>
2020-06-09 09:16:55 -0400

    Fixed bug in loading predictions from file with ndim != 4

--- src/segmentation_io.jl
+++ src/segmentation_io.jl
@@ -13,13 +13,16 @@ end
 
 """
 Loads UNet predictions in 3D dataset `h5_file::String`.
-Assumes predictions are stored in the fourth dimension of the file.
+If the file has a fourth dimension, assumes predictions are stored in its second entry.
 Returns the portion of `predictions` field corresponding to foreground.
 Can optionally set `threshold` to binarize predictions.
 """
 function load_predictions(h5_file::String; threshold=nothing)
     prediction_set = h5open(h5_file, "r")
-    predictions = read(prediction_set, "predictions")[:,:,:,2]
+    predictions = read(prediction_set, "predictions")
+    if length(size(predictions)) == 4
+        predictions = predictions[:,:,:,2]
+    end
     close(prediction_set)
     if threshold == nothing
         return predictions
