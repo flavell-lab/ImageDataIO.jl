5b6d24677f84b0de2cd97240700cfbea2ec16b1d
urlicht <jungsu93@gmail.com>
2020-12-14 18:37:56 -0500

    add FlavellBase

--- src/ImageDataIO.jl
+++ src/ImageDataIO.jl
@@ -1,6 +1,6 @@
 module ImageDataIO
 
-using HDF5, MHDIO, ProgressMeter, GPUFilter
+using FlavellBase, HDF5, MHDIO, ProgressMeter, GPUFilter
 
 include("centroids_io.jl")
 include("worm_features_io.jl")
