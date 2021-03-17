26332ee83b973de40c0467ebd1a11728ba5abba8
Jungsoo Kim <urlicht@users.noreply.github.com>
2021-03-16 21:49:20 -0400

    Update shear_correction.jl
    modify shear_correction_mhd! to support subpixel registration

--- src/shear_correction.jl
+++ src/shear_correction.jl
@@ -12,31 +12,26 @@ Applies shear correction to a dataset.
  - `mhd_out_key::String` (optional): Key in `param_path` containing output MHD directory
  - `MIP_out_key::String` (optional): Key in `param_path` containing output MIP directory
 """
-function shear_correction_mhd!(param_path::Dict, param::Dict, ch_list, shear_params_dict::Dict;
+function shear_correction_mhd!(param_path::Dict, param::Dict, ch_marker::Int, ch_activity::Int, shear_param_dict::Dict;
         vmax::Int=1600, mhd_key::String="path_dir_mhd", mhd_out_key::String="path_dir_mhd_shearcorrect", MIP_out_key::String="path_dir_MIP_shearcorrect")
-
     create_dir(param_path[mhd_out_key])
     create_dir(param_path[MIP_out_key])
     size_x, size_y, size_z = length(param["x_range"]), length(param["y_range"]), length(param["z_range"])
     
-    img1_g = CuArray{Float32}(undef, size_x, size_y)
-    img2_g = CuArray{Float32}(undef, size_x, size_y)
     img1_f_g = CuArray{Complex{Float32}}(undef, size_x, size_y)
     img2_f_g = CuArray{Complex{Float32}}(undef, size_x, size_y)
-    CC_g = CuArray{Complex{Float32}}(undef, size_x, size_y)
+    CC2x_g = CuArray{Complex{Float32}}(undef, 2 * size_x, 2 * size_y)
     N_g = CuArray{Float32}(undef, size_x, size_y);
-
     img_stack = zeros(Float32, size_x, size_y, size_z)
     img_stack_reg = zeros(Float32, size_x, size_y, size_z)
-
 
     @showprogress for t = param["t_range"]
         # check if parameters exist at the given time point
         if !haskey(shear_params_dict, t)
-            shear_params_dict[t] = Dict()
+            shear_param_dict[t] = Dict()
         end
         # first channel in list does the registration; the other channels use the same one
-        for ch = ch_list
+        for ch = [ch_marker, ch_activity]
             basename = param_path["get_basename"](t, ch)
             path_mhd_in = joinpath(param_path[mhd_key], basename * ".mhd")
             path_mhd_out = joinpath(param_path[mhd_out_key], basename * ".mhd")
@@ -44,7 +39,7 @@ function shear_correction_mhd!(param_path::Dict, param::Dict, ch_list, shear_par
             path_MIP_out = joinpath(param_path[MIP_out_key], basename * ".png")
 
             img_stack .= Float32.(read_img(MHD(path_mhd_in)))
-            reg_stack_translate!(img_stack, img_stack_reg, img1_g, img2_g, img1_f_g, img2_f_g, CC_g, N_g, z_center=nothing, reg_params=shear_params_dict[t])
+            reg_stack_translate!(img_stack, img_stack_reg, img_stack_reg_g, img1_f_g, img2_f_g, CC2x_g, N_g, reg_params=shear_param_dict[t])
 
             cp(path_mhd_in, path_mhd_out, force=true)
             write_raw(path_raw_out, floor.(UInt16, clamp.(img_stack_reg, typemin(UInt16), typemax(UInt16))))
@@ -53,13 +48,13 @@ function shear_correction_mhd!(param_path::Dict, param::Dict, ch_list, shear_par
     end
  
     CUDA.unsafe_free!(img1_g)
-    CUDA.unsafe_free!(img2_g)
+    CUDA.unsafe_free!(img_stack_reg_g)
     CUDA.unsafe_free!(img1_f_g)
     CUDA.unsafe_free!(img2_f_g)
-    CUDA.unsafe_free!(CC_g)
+    CUDA.unsafe_free!(CC2x_g)
     CUDA.unsafe_free!(N_g)    
-    CUDA.reclaim()
     GC.gc(true)
+    CUDA.reclaim()
  
     return shear_params_dict
 end
