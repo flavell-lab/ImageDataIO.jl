c2f3c4e52352b31d182487fc41463600b24bd454
Adam Atanas <goldenratio1618@users.noreply.github.com>
2021-03-18 19:06:01 -0400

    Fix typos, require one channel

--- src/shear_correction.jl
+++ src/shear_correction.jl
@@ -4,15 +4,15 @@ Applies shear correction to a dataset.
 # Arguments
  - `param_path::Dict`: Dictionary containing locations of input and output directories with MHD files
  - `param::Dict`: Dictionary containing image dimension parameters `x_range`, `y_range`, `z_range`, and `t_range`
- - `ch_list`: List of channels to apply shear correction to; all channels will have the same shear correction applied.
+ - `ch`: Channel to apply shear correction to
  - `shear_params_dict::Dict`: Dictionary of shear correction parameters. If nonempty, those parameters will be used.
     If empty, it will be filled with the computed paramers.
- - `vmax::Int`: Contrast parameter for png files
+ - `vmax::Int` (optional, default 1600): Contrast parameter for png files
  - `mhd_key::String` (optional): Key in `param_path` containing input MHD directory
  - `mhd_out_key::String` (optional): Key in `param_path` containing output MHD directory
  - `MIP_out_key::String` (optional): Key in `param_path` containing output MIP directory
 """
-function shear_correction_mhd!(param_path::Dict, param::Dict, ch_marker::Int, ch_activity::Int, shear_param_dict::Dict;
+function shear_correction_mhd!(param_path::Dict, param::Dict, ch::Int, shear_params_dict::Dict;
         vmax::Int=1600, mhd_key::String="path_dir_mhd", mhd_out_key::String="path_dir_mhd_shearcorrect", MIP_out_key::String="path_dir_MIP_shearcorrect")
     create_dir(param_path[mhd_out_key])
     create_dir(param_path[MIP_out_key])
@@ -28,23 +28,20 @@ function shear_correction_mhd!(param_path::Dict, param::Dict, ch_marker::Int, ch
     @showprogress for t = param["t_range"]
         # check if parameters exist at the given time point
         if !haskey(shear_params_dict, t)
-            shear_param_dict[t] = Dict()
+            shear_params_dict[t] = Dict()
         end
-        # first channel in list does the registration; the other channels use the same one
-        for ch = [ch_marker, ch_activity]
-            basename = param_path["get_basename"](t, ch)
-            path_mhd_in = joinpath(param_path[mhd_key], basename * ".mhd")
-            path_mhd_out = joinpath(param_path[mhd_out_key], basename * ".mhd")
-            path_raw_out = joinpath(param_path[mhd_out_key], basename * ".raw")
-            path_MIP_out = joinpath(param_path[MIP_out_key], basename * ".png")
+        basename = param_path["get_basename"](t, ch)
+        path_mhd_in = joinpath(param_path[mhd_key], basename * ".mhd")
+        path_mhd_out = joinpath(param_path[mhd_out_key], basename * ".mhd")
+        path_raw_out = joinpath(param_path[mhd_out_key], basename * ".raw")
+        path_MIP_out = joinpath(param_path[MIP_out_key], basename * ".png")
 
-            img_stack .= Float32.(read_img(MHD(path_mhd_in)))
-            reg_stack_translate!(img_stack, img_stack_reg, img_stack_reg_g, img1_f_g, img2_f_g, CC2x_g, N_g, reg_param=shear_param_dict[t])
+        img_stack .= Float32.(read_img(MHD(path_mhd_in)))
+        reg_stack_translate!(img_stack, img_stack_reg, img_stack_reg_g, img1_f_g, img2_f_g, CC2x_g, N_g, reg_param=shear_params_dict[t])
 
-            cp(path_mhd_in, path_mhd_out, force=true)
-            write_raw(path_raw_out, floor.(UInt16, clamp.(img_stack_reg, typemin(UInt16), typemax(UInt16))))
-            imsave(path_MIP_out, maxprj(img_stack_reg, dims=3) / vmax, cmap="gray")
-        end
+        cp(path_mhd_in, path_mhd_out, force=true)
+        write_raw(path_raw_out, floor.(UInt16, clamp.(img_stack_reg, typemin(UInt16), typemax(UInt16))))
+        imsave(path_MIP_out, maxprj(img_stack_reg, dims=3) / vmax, cmap="gray")
     end
  
     CUDA.unsafe_free!(img1_g)
