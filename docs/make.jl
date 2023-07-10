ee73b1dec9a102d92f599195487a95bb6da62298
Adam Atanas <adamatanas@gmail.com>
2023-07-10 17:32:30 -0400

    Update documentation workflow

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, ImageDataIO
 
 makedocs(sitename="ImageDataIO")
 
-deploydocs(repo="github.com/flavell-lab/ImageDataIO.jl.git", devbranch="master")
+deploydocs(repo="github.com/flavell-lab/ImageDataIO.jl.git", push_preview=false)
